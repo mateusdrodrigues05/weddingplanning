73e3f63937ff1e3ec31b0b6140d1c67c84db7358
Debian <debian@vps-mateus.fernandomarcos.me>
2026-09-20 14:15:50 +0000

    fix some routes

--- routes/api.php
+++ routes/api.php
@@ -5,7 +5,7 @@ use App\Http\Controllers\Api\GuestController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->group(function () {
-    Route::apiResource('guests', GuestController::class)->parameters([
-        'guests' => 'guest:id'
-    ]);
+    Route::apiResource('guests', GuestController::class)
+        ->parameters(['guests' => 'guest:id'])
+        ->names('api.guests');
 });
